89f0c3d895c9aa9dfd281a1c8bed900e686cc963
Wonyeong Choi <coma01155@gmail.com>
2021-08-07 16:03:07 +0900

    chore: site meta 정보 수정

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,32 +1,42 @@
 import {css} from "@emotion/react";
 
 export const TableWrapper = css`
-  @media screen and (min-width: 601px) {
-    min-width: 80vh;
+  @media screen and (min-width: 360px) {
+    min-width: 30vh;
   }
-`
 
-export const titleWrapper = css`
-  font-family: 'Work Sans', sans-serif;
-  @media screen and (min-width: 601px) {
-    font-size: 5vw;
-  }
-  @media screen and (max-width: 600px) {
-    font-size: 9vw;
+  @media screen and (min-width: 425px) {
+    min-width: 40vh;
   }
   
+  @media screen and (min-width: 768px) {
+    min-width: 60vh;
+  }
+`
+export const titleWrapper = css`
+  font-family: 'Work Sans', sans-serif;
   color: #EDEBEC;
+  
+  @media screen and (min-width: 360px) {
+    font-size: 3vw;
+  }
+  
+  @media screen and (min-width: 426px) {
+    font-size: 4vw;
+  }
 `
 
 export const buttonWrapper = css`
   font-family: 'Open Sans Condensed', sans-serif;
   color: #FFF;
-  @media screen and (min-width: 601px) {
-    width: 25vw;
-    height: 4vw;
+
+  @media screen and (min-width: 360px) {
+    width: 26vw;
+    height: 6vw;
   }
-  @media screen and (max-width: 600px) {
-    width: 50vw;
-    height: 9vw;
+  
+  @media screen and (min-width: 426px) {
+    width: 30vw;
+    height: 6vw;
   }
 `
